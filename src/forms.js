de7d4c5b5cad86f923f03e97bc4f398eca1346be
James <jwljacl@gmail.com>
2020-01-20 23:30:23 -0500

    rev2

--- src/forms.js
+++ src/forms.js
@@ -1,4 +1,4 @@
-export function forms(projectControl, pArray) {
+export function forms(projectControl, pArray, projClicked, tArray) {
     document.querySelector(".add").addEventListener("click", function(){
         document.getElementById("myForm").style.display = "block";
     })
@@ -12,23 +12,88 @@ export function forms(projectControl, pArray) {
         document.getElementById("myProject").style.display = "none";
     })
     
+    /*
     document.querySelector(".btn").addEventListener("click", function(e) {
         e.preventDefault();
     })
+    */
     
     document.querySelector(".pBtn").addEventListener("click", function(e) {
         e.preventDefault();
 
         let projInput = document.getElementById("pTitleIn").value;
         let newProject = projectControl(projInput);
+        pArray.push(newProject);
 
         let newDiv = document.createElement("div");
-        let projParent = document.getElementsByClassName("projects");
+        let projParent = document.querySelector(".projects");   //again, getElementsByClassNames doesn't work here (IDK why?)
+        projParent.appendChild(newDiv); //STUCK: appendChild not a function?? in console.
         newDiv.id = "project";
         newDiv.textContent = newProject.name;
-        projParent.appendChild(newDiv); //STUCK: appendChild not a function?? in console.
 
-        pArray.push(newProject);
-        console.log(pArray);
+        //LEARNED: use buttons instead of adding event listener to an innerHTML
+        let delP = document.createElement("button");
+        newDiv.appendChild(delP);
+        delP.id = "deleteP";
+        delP.textContent = "X";
+
+        //NOTE: since under parent "submit" function, any delete will delete that element.
+        delP.addEventListener("click", function(e) {
+            pArray.splice(pArray.length-1, 1);
+            newDiv.remove();
+        })
     })
+
+    //click what?
+    document.querySelectorAll("#project").map(a => {
+        a.addEventListener("click", function() {
+            document.querySelector(".btn").addEventListener("click", function(e) {  //to-do submit
+                e.preventDefault();
+                //assign value to each to-do parent item as a.textContent to categorize each item under the chosen project.
+                let tTitle = document.querySelector("#titleIn").value;
+                let tNotes = document.querySelector("#notesIn").value;
+                let tPriority = document.querySelector("#priorityIn").value;
+
+                //do title & notes first, then figure out priority & dates later.
+                let newToDo = pToDo(tTitle, tNotes);
+                tArray.push(newToDo);
+
+                let tParent = document.querySelector(".todo");
+                let newTDiv = document.createElement("div");
+                tParent.appendChild(newTDiv);
+                newTDiv.id = "item";
+                newTDiv.value = a.textContent;
+
+                let title = document.createElement("div");
+                title.id = "title";
+                title.textContent = tTitle;
+                newTDiv.appendChild(title);
+
+                let notes = document.createElement("div");
+                notes.id = "notes";
+                notes.textContent = tNotes;
+                newTDiv.appendChild(notes);
+
+                let delT = document.createElement("button");
+                newTDiv.appendChild(delT);
+                delT.id = "deleteI";
+                delT.textContent = "X";
+
+                delT.addEventListener("click", function(e) {
+                    tArray.splice(pArray.length-1, 1);
+                    newTDiv.remove();
+                })
+            })
+            //add below: to-do map, if (a.textContent == to-do.value) {style.display = "block"}
+            let item = document.querySelectorAll("item");
+            item.map(b => {
+                if (b.value == a.textContent) {
+                    b.style.display = "block";
+                } else {
+                    b.style.display = "none";
+                }
+            })
+        })
+    })
+
 }
--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ FILES:
 
 LEARNED:
     - onclick & getElementById for openForm/closeForm don't work - querySelector works.
+    - childNode.remove(): removes element from DOM tree.
 
 STATUS:
     - create HTML skeleton (use JS later) - COMPLETE
-    - create input forms & get data.
+    - create input forms & get data. - COMPLETE
+    - work: when project is clicked, add/categorize/show to-do items.
+    - organize CSS
 */
 
 import { forms } from "./forms.js"
@@ -41,8 +44,12 @@ function renderControl() {
     //must create input form.
     //let project1 = Object.create(projectControl(name of project)) OR create function inside the FF that takes in all arguments.
 
-    const pArray = [];  //stores to-do
-    forms(projectControl, pArray);  //MUST pass through these as arguments.
+    //initial click checker: false - unless project is "clicked", then activated to "true".
+    var projClicked = false;
+
+    const pArray = [];  //stores projects
+    const tArray = [];  //stores to-dos
+    forms(projectControl, pArray, projClicked, tArray);  //MUST pass through these as arguments.
 
 }
 
